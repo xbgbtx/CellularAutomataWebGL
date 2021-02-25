3201890c0218123e80aaa521c606db839424cef9
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-02-25 11:45:04 +0000

    refactor

--- ca_pixi.js
+++ ca_pixi.js
@@ -2,6 +2,21 @@ class CAPixi
 {
     constructor ()
     {
+        this.create_pixi_app ();
+        this.create_sprites ();
+
+        this.pixi_app.stage.addChild ( this.display_sprite );
+
+        this.draw_queue = [];
+
+        this.pixi_app.ticker.add ( () => this.update () );
+
+        document.getElementById ( "canvas_div" )
+            .appendChild ( this.pixi_app.view );
+    }
+
+    create_pixi_app () 
+    {
         let type="WebGL";
 
         if ( !PIXI.utils.isWebGLSupported () )
@@ -16,19 +31,21 @@ class CAPixi
             width  : w,
             height : h
         });
+    }
 
-        let tOpt = 
-        {
-            width : w,
-            height : h,
-            wrapMode : PIXI.WRAP_MODES.REPEAT,
-            mipmap : false,
-        };
-        
+    create_sprites ()
+    {
         let make_render_texture = () =>
         {
+            let tOpt = 
+            {
+                width : w,
+                height : h,
+                wrapMode : PIXI.WRAP_MODES.REPEAT,
+                mipmap : false,
+            };
+
             let brt = new PIXI.BaseRenderTexture(tOpt)
-            //brt.wrapMode = PIXI.WRAP_MODES.REPEAT;
             let rt = new PIXI.RenderTexture ( brt )
             return rt;
         }
@@ -36,40 +53,33 @@ class CAPixi
         let t0 = make_render_texture ();
         let t1 = make_render_texture ();
 
+        //draw sprite is blitted onto display sprite and then textures
+        //are swapped
         this.draw_sprite = PIXI.Sprite.from ( t0 );
         this.display_sprite = PIXI.Sprite.from ( t1 );
-
-        this.pixi_app.stage.addChild ( this.display_sprite );
-
-        this.draw_queue = [];
-
-
-        this.pixi_app.ticker.add ( () =>
-        {
-            for ( const d of this.draw_queue ) 
-            {
-                this.pixi_app.renderer.render ( d,this. draw_sprite.texture );
-            }
-
-            this.draw_queue = [];
-
-            this.pixi_app.renderer.render ( 
-                this.draw_sprite, this.display_sprite.texture );
-
-            //swap textures
-            let tmp = this.draw_sprite.texture;
-            this.draw_sprite.texture = this.display_sprite.texture;
-            this.display_sprite.texture = tmp;
-        });
-
-        document.getElementById ( "canvas_div" )
-            .appendChild ( this.pixi_app.view );
     }
-
 
     set_active_shader ( shaderCode )
     {
         let shader = new PIXI.Filter ( undefined, shaderCode, {} );
         this.draw_sprite.filters = [ shader ];
+    }
+
+    update ()
+    {
+        for ( const d of this.draw_queue ) 
+        {
+            this.pixi_app.renderer.render ( d, this.draw_sprite.texture );
+        }
+
+        this.draw_queue = [];
+
+        this.pixi_app.renderer.render ( 
+            this.draw_sprite, this.display_sprite.texture );
+
+        //swap textures
+        let tmp = this.draw_sprite.texture;
+        this.draw_sprite.texture = this.display_sprite.texture;
+        this.display_sprite.texture = tmp;
     }
 }
